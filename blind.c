659b0913c0fb4b50e4c5857c8a86de2e353d141b
Paul Fox <pgf@laptop.org>
2013-03-19 16:00:31 -0400

    blind: add hardware definitions

--- blind.c
+++ blind.c
@@ -10,11 +10,19 @@
 
 char cur_rotation;
 
+#define PORTMOTOR PORTA
+#define PINMOTOR PINA
+#define DDRMOTOR DDRA
+#define P_MOTOR_ON	PA0
+#define P_MOTOR_DIR	PA1
+#define P_MOTOR_TURN	PA2
+#define P_LIMIT		PA3
+
 #define NOT_MOVING 0
 #define MOVING_UP 1
 #define MOVING_DOWN 2
-#define STOPPING 2
-char moving;
+#define BOUNCEBACK 2
+char motion;
 char atpeak;
 
 #define LEAVING_LIMIT 10
@@ -36,31 +44,32 @@ get_local_cmd(void)
     return 0;
 }
 
-char
-get_serial_cmd(void)
-{
-    return 0;
-}
-
-
 /* I/O -- read the limit switch, control the motors */
 int
 hitlimit(void)
 {
     /* detect the limit switch */
-    return 0;
+    return PINMOTOR & bit(P_LIMIT);
 }
 
 void
 set_motion(int on)
 {
     /* set motor run state bit */
+    if (on)
+	PORTMOTOR |= bit(P_MOTOR_ON);
+    else
+	PORTMOTOR &= ~bit(P_MOTOR_ON);
 }
 
 void
 set_direction(int cw)
 {
     /* set rotation bit */
+    if (cw)
+	PORTMOTOR |= bit(P_MOTOR_DIR);
+    else
+	PORTMOTOR &= ~bit(P_MOTOR_DIR);
 }
 
 /*
@@ -74,13 +83,21 @@ set_direction(int cw)
 void
 blind_init(void)
 {
+    PORTMOTOR &= ~(bit(P_MOTOR_ON) | bit(P_MOTOR_DIR));
+    DDRMOTOR |= bit(P_MOTOR_ON) | bit(P_MOTOR_DIR);
+
+    // clear and enable motor rotation interrupt
+    GIFR = bit(INTF1);
+    GIMSK |= bit(INT1);
+
     set_motion(0);
+    set_direction(0);
     pulses = pulsegoal = LOW_IDLE;
     cur_rotation = 0;
 
     peak = NOMINAL_PEAK;
+    motion = NOT_MOVING;
     atpeak = 0;
-    moving = NOT_MOVING;
 }
 
 void
@@ -91,36 +108,37 @@ stop_moving(void)
 }
 
 void
-start_moving(int type)
+start_moving(void)
 {
     set_direction(cur_rotation);
     set_motion(1);
 }
 
-void
-pulse_isr(void)
+ISR(INT1_vect)		// limit switch
 {
     pulses++;
     if (hitlimit() && pulses > LEAVING_LIMIT) {
-	if (moving != STOPPING) {
+
+	if (motion != BOUNCEBACK) {
 	    stop_moving();
 	    cur_rotation = !cur_rotation;
 	}
-	if (moving == MOVING_DOWN) {
+
+	if (motion == MOVING_DOWN) {
 	    peak = pulses/2;
+	    // we want to move back up off the limit
 	    pulsegoal = LOW_IDLE;
-	    moving = STOPPING;
+	    motion = BOUNCEBACK;
 	}
-	start_moving(moving);
+	start_moving();
 	pulses = 0;
-	return;
-    } 
-    
-    if (moving == MOVING_UP || moving == STOPPING) {
+    } else  if (motion == MOVING_UP || motion == BOUNCEBACK) {
+
 	if (pulses >= pulsegoal) {
 	    stop_moving();
-	    moving = NOT_MOVING;
-	    atpeak = 1;
+	    if (motion == MOVING_UP)
+		atpeak = 1;
+	    motion = NOT_MOVING;
 	}
     }
 }
@@ -186,11 +204,11 @@ void blind_process(void)
 	    break;
 
 	pulsegoal = peak;
-	start_moving(MOVING_UP);
+	start_moving();
 	break;
 
     case BL_GO_DOWN:
-	start_moving(MOVING_DOWN);
+	start_moving();
 	break;
     }
 }
